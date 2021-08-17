ecbfdb82b0cf63e94e16b9f58f1a451ffb04b8d9
Munna-Mahmud <learnermunna@gmail.com>
2021-08-17 20:01:36 +0600

    added a js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// SET BY FUNTION 
+document.getElementById('header-button').addEventListener('click', function(){
+    const headerTitle=document.getElementById('header-title');
+     headerTitle.innerText='I AM PENGUIN';
+
+});
